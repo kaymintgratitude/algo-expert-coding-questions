df6a504b1cc19fd13f1141af7e7204e478a01c57
Kenneth Mensah <kenneth.mensah62@gmail.com>
2020-09-26 13:25:07 +0100

    Added Python Solution for Two Number sum

---
+++ arrays/two_number_sum.py
@@ -0,0 +1,36 @@
+"""
+Problem:        Two Number Sum
+Description:    Write a function that take in a non-empty array of distinct integers and an integer
+                representing a target sum. If any two numbers in the input array sum up to the target sum,
+                the function should them in an array, in any order. If no two numbers sum up to the target sum,
+                the function should return an empty array. 
+
+                Note that the target sum has to be obtained by summing two different integers in the array; you 
+                can't add a single integer to itself to obtain the target sum. 
+
+                You can assume that there will be at most one pair of numbers summing up to the target sum.
+
+                Sample Input: 
+                array = [ 3, 5, -4, 8, 11, 1, -1, 6]
+                targetSum = 10
+
+                Sample Output:
+                [-1, 11]
+               
+"""
+
+
+def two_number_sum(arr, targetSum):
+    '''
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    '''
+    dedup_set = set()
+    for currentNum in arr:
+        difference = targetSum - currentNum
+        if difference in dedup_set:
+            return [ difference, currentNum]
+        dedup_set.add(currentNum)
+    
+    return []
+
