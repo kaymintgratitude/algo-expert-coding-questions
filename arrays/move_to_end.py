44926e4100e1fe7b9a95a38b39434bb72ee1bae3
Kenneth Mensah <kenneth.mensah62@gmail.com>
2020-09-27 00:58:50 +0100

    Added solution for move to end

---
+++ arrays/move_to_end.py
@@ -0,0 +1,53 @@
+"""
+Problem:        Move Element To End
+Description:    You're given an array of integers and an integer. Write a function
+                that moves all instances of that integer in the array to the end of the array
+                and returns the array.
+
+                The function should perform this in place (i.e., it should mutate the input array)
+                and doesn't need to maintain the order of the other integers.
+
+                Sample Input
+                array = [2, 1, 2, 2, 2, 3, 4, 2]
+                toMove = 2
+
+                Sample Output
+                [1, 3, 4, 2, 2, 2, 2, 2]
+
+"""
+
+import unittest
+
+
+def move_element_to_end(arr, to_move):
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        if arr[start] == to_move:
+            if arr[end] == to_move:
+                end -= 1
+            else:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+        else:
+            start += 1
+
+    return arr
+
+
+class TestSmallestDifference(unittest.TestCase):
+    def test_input_array_has_valid_move_to_end(self):
+        actual = move_element_to_end([2, 1, 2, 2, 2, 3, 4, 2], 2)
+        expected = [4, 1, 3, 2, 2, 2, 2, 2]
+        self.assertListEqual(expected, actual)
+
+    def test_input_array_has_invalid_move_to_end(self):
+        actual = move_element_to_end([1, 2, 3, 4, 5], 10)
+        expected = [1, 2, 3, 4, 5]
+        self.assertListEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
